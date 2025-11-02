2aa9ae278bbda664e1d3a574995b90ae08d4797b
gautierjf <gautierjf@gmail.com>
2025-11-02 21:44:50 +0100

    fix script sh

--- install.sh
+++ install.sh
@@ -9,11 +9,22 @@ sudo apt upgrade -y
 
 # Installer Node.js LTS depuis le dépôt officiel
 curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
-sudo apt install -y nodejs
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.6/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+nvm install 20
+nvm use 20
+nvm alias default 20
 
 echo "Node.js version: $(node -v)"
 echo "npm version: $(npm -v)"
 
+export NODE_OPTIONS="--max-old-space-size=512"
+
+echo ""
+echo "=== Copie fichier racine photobooth-backend ==="
+read
 echo ""
 echo "=== Configuration des variables d'environnement ==="
 
@@ -21,11 +32,11 @@ read -p "Entrez le chemin vers Google Credentials (GOOGLE_CREDENTIALS_PATH) : " 
 read -p "Entrez le JWT Secret (JWT_SECRET) : " JWT_SECRET
 read -p "Entrez le Folder ID (FOLDER_ID) : " FOLDER_ID
 
-cat > .env <<EOL
-GOOGLE_CREDENTIALS_PATH="$GOOGLE_CREDENTIALS_PATH"
+cat > photobooth-backend/.env <<EOL
+GOOGLE_CREDENTIALS_PATH=$GOOGLE_CREDENTIALS_PATH
 GOOGLE_SCOPES_API=https://www.googleapis.com/auth/drive
-JWT_SECRET="$JWT_SECRET"
-FOLDER_ID="$FOLDER_ID"
+JWT_SECRET=$JWT_SECRET
+FOLDER_ID=$FOLDER_ID
 REDIRECT_TO_LOGIN=http://localhost:3000/camera
 EOL
 
